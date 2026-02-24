bb8adb1cacd6cf87fb1123454e6e199822be61c9
luisgruber <luisgruber.mail@gmail.com>
2026-02-24 17:30:12 +0100

    add fun and test

---
+++ R/fun.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param x to
+#' @param y do
+#'
+#' @returns to
+#' @export
+#'
+#' @examples
+#' f(1,2)
+f <- function(x, y){
+  x+y
+}
---
+++ tests/testthat/test-fun.R
@@ -0,0 +1,4 @@
+test_that("f works", {
+  res <- f(1,1)
+  expect_equal(res, 2)
+})
